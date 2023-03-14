bf290370807e9f7995224271e6c541fd27c5f5a4
steven32323 <107620513+steven32323@users.noreply.github.com>
2023-03-14 09:38:09 +0000

    made expenses.js a bit leaner, moved code to ExpensesList.js

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './Expenses.css';
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpenseFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -14,20 +14,6 @@ const Expenses = props => {
     expense => expense.date.getFullYear() === parseInt(filteredYear)
   );
 
-  //sets a value to display when there are 0 results after filtering
-  let expensesContent = <p>No Expenses found.</p>;
-
-  // if there are results after filtering, iterates through and displays them using map
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map(expense => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
   return (
     <div>
       <Card className="expenses">
@@ -35,7 +21,7 @@ const Expenses = props => {
           selected={filteredYear}
           onFilterChange={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
