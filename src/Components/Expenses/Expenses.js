c51aa61bb5cd1bb241a6de472bc513b18c955f4f
steven32323 <107620513+steven32323@users.noreply.github.com>
2023-03-14 00:00:48 +0000

    Finished implementing filter functionality

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -10,6 +10,24 @@ const Expenses = props => {
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter(
+    expense => expense.date.getFullYear() === parseInt(filteredYear)
+  );
+
+  //sets a value to display when there are 0 results after filtering
+  let expensesContent = <p>No Expenses found.</p>;
+
+  // if there are results after filtering, iterates through and displays them using map
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(expense => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <div>
       <Card className="expenses">
@@ -17,26 +35,7 @@ const Expenses = props => {
           selected={filteredYear}
           onFilterChange={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {expensesContent}
       </Card>
     </div>
   );
